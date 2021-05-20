506bfb17bcac464e1c2e4e643c972933a44c6ef7
Major Hayden <major@mhtx.net>
2021-05-20 07:20:07 -0500

    Use bash to provision

--- Vagrantfile
+++ Vagrantfile
@@ -1,7 +1,5 @@
 Vagrant.configure("2") do |config|
     config.vm.box = "centos/stream8"
     config.vm.define 'centos'
-    config.vm.provision "ansible" do |ansible|
-        ansible.playbook = "playbook.yml"
-    end
+    config.vm.provision "shell", path: "provision.sh"
 end
---
+++ provision.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "fastestmirror=1" >> /etc/dnf/dnf.conf
+dnf -y upgrade
+dnf -y install composer-cli osbuild-composer
+usermod -a -G weldr vagrant
+systemctl enable --now osbuild-composer.socket
+for i in `seq 1 10`; do
+    composer-cli status show && break
+    sleep 1
+done
