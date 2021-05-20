819743f071acc8588c126d42b4777534548f6dfb
Major Hayden <major@mhtx.net>
2021-05-20 08:53:10 -0500

    Resize the filesystem inside the vagrant box

--- provision.sh
+++ provision.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
 echo "fastestmirror=1" >> /etc/dnf/dnf.conf
-# dnf -y upgrade
-dnf -y install composer-cli osbuild-composer jq
+dnf -y install cloud-utils-growpart composer-cli osbuild-composer jq
+
+df -hT
+growpart /dev/sda 1
+df -hT
+
 usermod -a -G weldr vagrant
 systemctl enable --now osbuild-composer.socket
+
 for i in `seq 1 10`; do
     composer-cli status show && break
     sleep 1
